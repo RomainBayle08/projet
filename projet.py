a33035701d3b7d5c2f728fe4b2e5da991ea50092
romainbayle <romain.bayle08@outlook.com>
2024-02-12 16:32:14 +0100

    remaniement de la fonction "best_fit_width_algo" pour qu'elle soit comme demander

--- projet.py
+++ projet.py
@@ -106,24 +106,81 @@ def printConteneurs(w: int, h: int, donnees: list):
 def infinite_strip(list_rect):# retourne un conteneur (Box)
     etage = 0
     conteneur = test.box(1000,10)
-    while len(list_rect)> 0:
-        current = list_rect.pop(0)
-        best_fit_width_algo(list_rect, current, conteneur, etage)
-        etage += 1
 
+    while len(list_rect)>0:
+        current = list_rect.pop(0)
+        etage = best_fit_width_algo(current, conteneur, etage)
     return conteneur
 
-def best_fit_width_algo(list_rect, current, conteneur, etage):
-    conteneur.add(etage, current.h, current.w)
-    remaing_W = conteneur.W - current.w
+def best_fit_width_algo(current, conteneur, etage):
+    remaing_W = 10 # compteur de place restante
+    etage_to_check = 0 # etage parcours
+    is_fited = False   # l'objet courrant ( current) a été placer
 
+    if etage == 0:  # si on est sur le premier etage
+        if conteneur.list_contain: # si on a deja des elements
+            for rect in conteneur.list_contain: # on calcul l'espace residuel
+                remaing_W = remaing_W - rect[2]
+        if remaing_W < current.w: # si on a pas de place on passe a l'etage suivant ( le 1 )
+            etage = etage + 1
+        conteneur.add(etage, current.h,current.w) # on ajoute le bloc courant a l'etage souhaiter
+        return etage
+
+    else:  # si on est au 2ieme etage ou plus
+        for i in range(len(conteneur.list_contain)): # on passe tous les "rect" deja placer
+            rect = conteneur.list_contain[i]
+            if rect[0] > etage_to_check:  # si on change d'etage
+                if remaing_W >= current.w: # si suffisament de place pour l'object courant a l'etage n-1
+                    conteneur.list_contain.insert(i, (etage_to_check, current.h, current.w)) # on l'ajoute a l'indice actuel
+                    is_fited = True # on dit que le bloc a été placer
+                    break
+                etage_to_check = rect[0] # vu qu'on a changer d'etage l'etage actuel deviens l'etage a l'indice i
+                remaing_W = 10 # on remet a 0 le compteru de place restante
+            remaing_W -= rect[2] # on soustrait la largeur du "rect" actuel au compter
+        etage_to_check += 1
+        if not is_fited:
+            conteneur.add(etage_to_check,current.h,current.w) # si l'objet courant n'a pas pu etre placer il est placer dans un nouvel etage d'ou le etage_to_check +1
+        return etage_to_check
+
+
+
+
+
+
+
+
+
+    """
+    if etage !=0:
+        for i in range(len(conteneur.list_contain)):
+            rect = conteneur.list_contain[i]
+            if rect[0] > etage_to_check and rect[0] >0:
+                if remaing_W >= current.w:
+                    conteneur.list_contain.insert(i-1,(etage_to_check,current.h,current.w))
+                    is_fited = True
+                    break
+                etage_to_check = rect[0]
+                remaing_W = 0
+            remaing_W -= rect[2]
+    else:
+        for i in range(len(conteneur.list_contain)):
+            rect = conteneur.list_contain[i]
+            remaing_W-= rect[2]
+        if remaing_W >= current.w:
+            conteneur.add(etage,current.h,current.w)
+
+    if not is_fited :
+        conteneur.add(etage,current.h,current.w)
+
+    
     while remaing_W > 0:
-        min = findMinWidth(list_rect)
-        if remaing_W - min.w < 0:
+        next_element = list_rect[0]
+        if remaing_W - next_element.w < 0:
             break
         else:
-            conteneur.add(etage, min.h, min.w)
-            remaing_W -= min.w
+            conteneur.add(etage, next_element.h, next_element.w)
+            list_rect.remove(next_element)
+            remaing_W -= next_element.w"""
 
 def findMinWidth(listRect):
     min_rect = test.rect(1000000, 1000000)
@@ -156,8 +213,10 @@ list_rect = convert_sorted_to_object_list(triHauteur(donnees))
 
 infinite_cont = infinite_strip(list_rect)
 
-etage_to_cont = test.etage_to_cont(infinite_cont)
+#etage_to_cont = test.etage_to_cont_raw(infinite_cont)
 
+
+#opti = test.etage_to_opti_cont(etage_to_cont)
 #best_fit_strip_test = test.best_fit_strip_algo(etage_to_cont)
 
 
@@ -167,3 +226,4 @@ if __name__ == '__main__':
 
     for rect in infinite_cont.list_contain:
         print(rect)
+    print('\n')
--- test.py
+++ test.py
@@ -29,13 +29,6 @@ class rect:
 
 
 
-def findMinWidth(listRect):
-    min_rect = rect(1000000, 1000000)
-    for r in listRect:
-        if r.w < min_rect.w:
-            min_rect = r
-    listRect.remove(min_rect)
-    return min_rect
 
 
 
@@ -56,7 +49,7 @@ def fill_cont(list_rect, conteneur_H,conteneur_W):
     remaing_H = conteneur_H - current.h
     conteneur = box(conteneur_H,conteneur_W)
     while remaing_H>=0:
-        best_fit_width_algo(list_rect, current, conteneur, etage)
+       # projet.best_fit_width_algo(list_rect, current, conteneur, etage)
         etage += 1
         if len(list_rect)>0:
             current = list_rect.pop(0)
@@ -69,20 +62,41 @@ def fill_cont(list_rect, conteneur_H,conteneur_W):
 
 
 
-def etage_to_cont(infinite_cont):
+def etage_to_cont_raw(infinite_cont):
     list_cont = []
     current_etage = 0;
     current_cont = box(10,10)
     remaing_h = 0
     for rect in infinite_cont.list_contain:
-        if rect[0] != current_etage:
-            remaing_h = 10 - rect[1]
+
+        if rect[0] > current_etage  :
+            remaing_h = 10- current_cont.list_contain[0][1]
             list_cont.append((remaing_h, current_cont))
             current_cont = box(10, 10)
             current_etage = rect[0]
         current_cont.add(0,rect[1],rect[2])
-    list_cont.pop(0)
     return list_cont
+
+
+def etage_to_opti_cont(list_cont):
+    list_opti_cont=[]
+    remaing_h_current_cont = 0
+    while len(list_cont) > 0:
+        cont = list_cont.pop(0)
+        remaing_h_current_cont = cont[0]
+        current_cont = cont[1]
+        closest_match = find_closest_match(list_cont,remaing_h_current_cont)
+        if remaing_h_current_cont>0 and len(closest_match.list_contain)>0:
+            current_cont.list_contain.extend(closest_match.list_contain)
+        list_opti_cont.append(current_cont)
+    return list_opti_cont
+
+def find_closest_match(list_cont , remaing_h):
+    closest_match = box(10,10)
+    for cont in list_cont:
+        if cont[0] <= remaing_h:
+            closest_match.list_contain = cont[1].list_contain
+    return closest_match
 
 """def best_fit_strip_algo(list_cont):
     list_cont_updated = []
@@ -104,20 +118,6 @@ def etage_to_cont(infinite_cont):
 
 
 
-
-
-
-def best_fit_width_algo(list_rect, current, conteneur, etage):
-    conteneur.add(etage, current.h, current.w)
-    remaing_W = conteneur.W - current.w
-
-    while remaing_W > 0:
-        min = findMinWidth(list_rect)
-        if remaing_W - min.w < 0:
-            break
-        else:
-            conteneur.add(etage, min.h, min.w)
-            remaing_W -= min.w
 
 
 
